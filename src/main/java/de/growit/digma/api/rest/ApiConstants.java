c4baaae86e2286c5a509195b30c23b3637204908
André Peglow <andre.peglow@gmx.net>
2012-01-29 22:03:31 +0100

    initial import

---
+++ src/main/java/de/growit/digma/api/rest/ApiConstants.java
@@ -0,0 +1,12 @@
+package de.growit.digma.api.rest;
+
+public class ApiConstants {
+	/**
+	 * The header name containing the repository id.
+	 */
+	@Deprecated
+	public final static String REPO_ID_HEADER = "X-DIGMA-RepositoryId"; //$NON-NLS-1$
+	
+	public final static String PROVIDER_TOKEN_HEADER = "X-DIGMA-PROVIDER-TOKEN"; //$NON-NLS-1$
+	
+}

ffd6e5cc1c7fcb4c0b5e122178888b276d70adfb
André Peglow <andre.peglow@gmx.net>
2012-02-12 12:50:46 +0100

    urls with patterns

--- src/main/java/de/growit/digma/api/rest/Urls.java
+++ src/main/java/de/growit/digma/api/rest/Urls.java
@@ -2,18 +2,32 @@ package de.growit.digma.api.rest;
 
 public class Urls {
 	
-	public static String forRootElements() {
-		return "/";
+	public static final String ROOT_ELEMENTS = "/";
+	public static final String CHILD_CATEGORIES = "/{parentId}/children/";
+	public static final String DOCUMENTS = "/categories/{categoryId}/documents/";
+	public static final String CATEGORY = "/{categoryId}/";
+	public static final String DOCUMENT = "/documents/{documentId}/";
+	
+	
+	public static String get(String path, Object...params) {
+		StringBuilder sb = new StringBuilder();
+		int currentParam = 0;
+		int lastPlaceHolderEnd = -1;
+		int placeHolderStart = path.indexOf("{");
+		while (placeHolderStart>-1) {
+			sb.append(path.substring(lastPlaceHolderEnd+1, placeHolderStart));			
+			lastPlaceHolderEnd = path.indexOf("}", placeHolderStart);			
+			sb.append(params[currentParam++]);
+			placeHolderStart = path.indexOf("{", lastPlaceHolderEnd);
+		}
+		sb.append(path.substring(lastPlaceHolderEnd+1, path.length()));
+		
+		return sb.toString();
 	}
 	
-	public static String forChildCategories(long parentId) {
-		return "/"+parentId+"/children/";
+	public static void main(String[] args) {
+		System.out.println(get(DOCUMENTS, 123));
 	}
-	
-	public static String forDocuments(long categoryId) {
-		return "/categories/"+categoryId+"/documents/";
-	}
-	
 	
 	
 
